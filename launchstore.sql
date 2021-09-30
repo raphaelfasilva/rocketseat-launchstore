dfcc49be6d2dda1b92cec85307677dc093de633c
raphaelfasilva <raphaelfasilva@gmail.com>
2021-09-29 22:06:21 -0300

    modificações 29/09/21

---
+++ launchstore.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "products" (
+  "id" SERIAL PRIMARY KEY,
+  "category_id" int UNIQUE,
+  "user_id" int UNIQUE,
+  "name" text,
+  "description" text,
+  "old_price" int,
+  "price" int,
+  "quantity" int,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "categories" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text
+);
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text,
+  "products_id" int UNIQUE
+);
+
+ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+
+ALTER TABLE "files" ADD FOREIGN KEY ("products_id") REFERENCES "products" ("id");
