58fa18a1fbe1215febaa1c34432bc68c16fb5f49
raphaelfasilva <raphaelfasilva@gmail.com>
2022-01-22 14:46:18 -0300

    modificações 22/01/22

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,39 @@ input.addEventListener("keydown", function(e) {
         e.target.value = value
     }, 1)
 })
+const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target
+        if (PhotosUpload.hasLimit(event, fileList)) return
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+                const div = PhotosUpload.getContainer(image)
+                PhotosUpload.preview.appendChild(div)
+            }
+            reader.readAsDataURL(file)
+        })
+    },
+    hasLimit(event) {
+        const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no maximo ${this.uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
+        div.onclick = () => alert('remover foto')
+        div.appendChild(image)
+        return div
+    }
+
+}
