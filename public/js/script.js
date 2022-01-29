564b56807e172b4fd48c3714616f09a661c6f5f2
raphaelfasilva <raphaelfasilva@gmail.com>
2022-01-29 14:00:27 -0300

    modificações 29/01/22

--- public/js/script.js
+++ public/js/script.js
@@ -34,9 +34,22 @@ const PhotosUpload = {
         PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
     hasLimit(event) {
-        const { uploadLimit, input: fileList } = PhotosUpload
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
         if (fileList.length > uploadLimit) {
             alert(`Envie no maximo ${this.uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+
+        })
+        const totalPhotos = fileList.length + photosDiv.length
+        if (totalPhotos > uploadLimit) {
+            alert("Vocẽ atingiu o limite máximo de fotos")
             event.preventDefault()
             return true
         }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('./middlewares/multer')
 const ProductsController = require("./app/controller/ProductsController")
 routes.get('/', ProductsController.index)
 routes.get('/ads/create', function(req, res) {
     return res.redirect("/products/create")
 })
 routes.get('/products/create', ProductsController.create)
-routes.post('/products', ProductsController.post)
-routes.put('/products', ProductsController.put)
+routes.post('/products', multer.array("photos", 6), ProductsController.post)
+routes.put('/products', multer.array("photos", 6), ProductsController.put)
 routes.get('/products/:id/edit', ProductsController.edit)
 routes.delete('/products', ProductsController.delete)
 module.exports = routes
