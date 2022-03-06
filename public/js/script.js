dc600139c5368bac5929ca25f599d96f24f33152
raphaelfasilva <raphaelfasilva@gmail.com>
2022-03-06 15:42:13 -0300

    modificações 06/03/22

--- public/js/script.js
+++ public/js/script.js
@@ -95,17 +95,3 @@ const PhotosUpload = {
     }
 
 }
-const ImageGallery = {
-    previews: document.querySelectorAll('.galery-preview img'),
-    setImage(event) {
-        const { target } = event
-        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
-        target.classList.add('active')
-        ImageGallery.highlight.src = target.src
-    }
-}
-const teste = {
-    getTeste() {
-        console.log("teste")
-    }
-}
